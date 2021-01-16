e400b1be7543d70d5a286f2c3ac1b1a8d572dd7f
Carmela Leung <54586536+carmelal@users.noreply.github.com>
2021-01-16 13:58:52 -0500

    Move boxes
    + Array of boxes, display only required
    + When click box, adds to screen

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 COLOURS = [['black', 'blue'], ['gray', 'green'], ['indigo', 'orange'], ['red', 'violet'], ['yellow', 'white']]
+@pair = rand(5)
+
+@boxes = []
 
 def tick args
-  pair = 1
-
   y = 250
-  COLOURS[pair].each_with_index do |colour, index|
+  COLOURS[@pair].each_with_index do |colour, index|
     args.outputs.sprites << [50, y + (index * 125), 100, 100, "sprites/square/#{colour}.png"]
   end
 
-  if args.inputs.mouse.button_left
-    if args.inputs.mouse.x.between?(50, 150) && args.inputs.mouse.y.between?(250, 350)
-      args.outputs.labels << [640, 500, "mouse clicked #{COLOURS[pair][0]} at #{args.inputs.mouse.point}"]
-    elsif args.inputs.mouse.x.between?(50, 150) && args.inputs.mouse.y.between?(375, 475)
-      args.outputs.labels << [640, 500, "mouse clicked #{COLOURS[pair][1]} at #{args.inputs.mouse.point}"]
+  @boxes.each_with_index { |box, index| args.outputs.sprites << [640, 500 - (200 * index), 200, 200, "sprites/square/#{box}.png"] unless 500 - (200 * index) < -200 }
+
+  if args.inputs.mouse.click
+    if args.inputs.mouse.point.inside_rect? [50, 250, 200, 200]
+      # args.outputs.labels << [640, 500, "mouse clicked #{COLOURS[@pair][0]} at #{args.inputs.mouse.point}"]
+      choice = 1
+    elsif args.inputs.mouse.point.inside_rect? [50, 375, 200, 200]
+      # args.outputs.labels << [640, 500, "mouse clicked #{COLOURS[@pair][1]} at #{args.inputs.mouse.point}"]
+      choice = 0
     end
+    @boxes.insert(0, COLOURS[@pair][choice])
+    @pair = rand(5)
   end
 end
